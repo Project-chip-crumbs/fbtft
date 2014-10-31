5acb3b71f3bf382a107a9d1083db213f6f8a7163
Wynter Woods <zerotri@gmail.com>
2014-10-30 22:19:13 -0700

    SEPS114A display working properly.

--- fb_seps114a.c
+++ fb_seps114a.c
@@ -80,133 +80,130 @@ static int init_display(struct fbtft_par *par)
 
 	if (par->gpio.reset != -1) {
 		gpio_set_value(par->gpio.reset, 0);
-		mdelay(10);
+		mdelay(5);
 		gpio_set_value(par->gpio.reset, 1);
-		mdelay(10);
+		mdelay(5);
 	}
+	if (par->gpio.latch != -1) {
+		gpio_set_value(par->gpio.latch, 0);
+	}
+
 
 	/*  Soft reset */
 	write_reg(par, SEPS114A_SOFT_RESET,0x00);      
-	
 	/* Standby ON/OFF*/
 	write_reg(par, SEPS114A_STANDBY_ON_OFF,0x01);          // Standby on
 	mdelay(5);                                           // Wait for 5ms (1ms Delay Minimum)
 	write_reg(par, SEPS114A_STANDBY_ON_OFF,0x00);          // Standby off
 	mdelay(5);                                           // 1ms Delay Minimum (1ms Delay Minimum)
-	
 	/* Display OFF */
 	write_reg(par, SEPS114A_DISPLAY_ON_OFF,0x00);
-	
 	/* Set Oscillator operation */
 	write_reg(par, SEPS114A_ANALOG_CONTROL,0x00);          // using external resistor and internal OSC
-	
 	/* Set frame rate */
-	write_reg(par, SEPS114A_OSC_ADJUST,0x03);              // frame rate : 95Hz
-	
+	write_reg(par, SEPS114A_OSC_ADJUST,0x00);              // frame rate : 95Hz
 	/* Set active display area of panel */
 	write_reg(par, SEPS114A_DISPLAY_X1,0x00);
 	write_reg(par, SEPS114A_DISPLAY_X2,0x5F);
 	write_reg(par, SEPS114A_DISPLAY_Y1,0x00);
 	write_reg(par, SEPS114A_DISPLAY_Y2,0x5F);
-	
 	/* Select the RGB data format and set the initial state of RGB interface port */
 	write_reg(par, SEPS114A_RGB_IF,0x00);                 // RGB 8bit interface
-	
 	/* Set RGB polarity */
 	write_reg(par, SEPS114A_RGB_POL,0x00);
-	
 	/* Set display mode control */
 	write_reg(par, SEPS114A_DISPLAY_MODE_CONTROL,0x80);   // SWAP:BGR, Reduce current : Normal, DC[1:0] : Normal
-	
 	/* Set MCU Interface */
 	write_reg(par, SEPS114A_CPU_IF,0x00);                 // MPU External interface mode, 8bits
-	
 	/* Set Memory Read/Write mode */
 	write_reg(par, SEPS114A_MEMORY_WRITE_READ,0x00);
-	
 	/* Set row scan direction */
 	write_reg(par, SEPS114A_ROW_SCAN_DIRECTION,0x00);     // Column : 0 --> Max, Row : 0 --> Max
-	
 	/* Set row scan mode */
 	write_reg(par, SEPS114A_ROW_SCAN_MODE,0x00);          // Alternate scan mode
-	
 	/* Set column current */
 	write_reg(par, SEPS114A_COLUMN_CURRENT_R,0x6E);
 	write_reg(par, SEPS114A_COLUMN_CURRENT_G,0x4F);
 	write_reg(par, SEPS114A_COLUMN_CURRENT_B,0x77);
-	
 	/* Set row overlap */
 	write_reg(par, SEPS114A_ROW_OVERLAP,0x00);            // Band gap only
-	
 	/* Set discharge time */
 	write_reg(par, SEPS114A_DISCHARGE_TIME,0x01);         // Discharge time : normal discharge
-	
 	/* Set peak pulse delay */
 	write_reg(par, SEPS114A_PEAK_PULSE_DELAY,0x00);
-	
 	/* Set peak pulse width */
 	write_reg(par, SEPS114A_PEAK_PULSE_WIDTH_R,0x02);
 	write_reg(par, SEPS114A_PEAK_PULSE_WIDTH_G,0x02);
 	write_reg(par, SEPS114A_PEAK_PULSE_WIDTH_B,0x02);
-	
 	/* Set precharge current */
 	write_reg(par, SEPS114A_PRECHARGE_CURRENT_R,0x14);
 	write_reg(par, SEPS114A_PRECHARGE_CURRENT_G,0x50);
 	write_reg(par, SEPS114A_PRECHARGE_CURRENT_B,0x19);
-	
 	/* Set row scan on/off  */
 	write_reg(par, SEPS114A_ROW_SCAN_ON_OFF,0x00);        // Normal row scan
-	
 	/* Set scan off level */
 	write_reg(par, SEPS114A_SCAN_OFF_LEVEL,0x04);         // VCC_C*0.75
-	
 	/* Set memory access point */
 	write_reg(par, SEPS114A_DISPLAYSTART_X,0x00);
 	write_reg(par, SEPS114A_DISPLAYSTART_Y,0x00);
-	
 	/* Display ON */
 	write_reg(par, SEPS114A_DISPLAY_ON_OFF,0x01);
 
+
 	write_reg(par, SEPS114A_MEMORY_WRITE_READ,0x02);
-
-	// Memory write
-	write_reg(par, SEPS114A_DDRAM_DATA_ACCESS_PORT);
-
-
-	char white[2] = {0xff,0xff};
-	int j=0;
-	for(j=0;j<9216;j++)
-	{
-		if (par->gpio.dc != -1)
-			gpio_set_value(par->gpio.dc, 1);
-		par->fbtftops.write(par, white, 2);
-	}
 
 	return 0;
 }
 
-static void set_addr_win(struct fbtft_par *par, int start_x, int start_y, int end_x, int end_y)
+static void set_addr_win(struct fbtft_par *par, int xs, int ys, int xe, int ye)
 {
 	fbtft_par_dbg(DEBUG_SET_ADDR_WIN, par,
-		"%s(xs=%d, ys=%d, xe=%d, ye=%d)\n", __func__, start_x, start_y, end_x, end_y);
+		"%s(xs=%d, ys=%d, xe=%d, ye=%d)\n", __func__, xs, ys, xe, ye);
 
 	write_reg(par, SEPS114A_MEMORY_WRITE_READ,0x02);
 	// Column address
-	write_reg(par, SEPS114A_MEM_X1, start_x);
-	write_reg(par, SEPS114A_MEM_X2, end_x);
+	write_reg(par, SEPS114A_MEM_X1, xs);
+	write_reg(par, SEPS114A_MEM_X2, xe);
 
 	// Row adress
-	write_reg(par, SEPS114A_MEM_Y1, start_y);
-	write_reg(par, SEPS114A_MEM_Y2, end_y);
+	write_reg(par, SEPS114A_MEM_Y1, ys);
+	write_reg(par, SEPS114A_MEM_Y2, ye);
 }
 
 static void seps114a_display_reset(struct fbtft_par *par) {
 	if (par->gpio.reset != -1) {
-		gpio_set_value(par->gpio.reset, 0);
-		mdelay(10);
-		gpio_set_value(par->gpio.reset, 1);
-		mdelay(10);
+		gpio_set_value(par->gpio.reset, 0);  /* Activate chip */
+		mdelay(5);
+		gpio_set_value(par->gpio.reset, 1);  /* Activate chip */
+		mdelay(5);
 	}
+}
+inline u16 swap_rgb (u16 rgb)
+{
+	return =	(((rgb << 8) & 0xff00) |
+				((rgb >> 8) & 0xff));
+}
+static int write_vmem(struct fbtft_par *par, size_t offset, size_t len)
+{
+	u16 *vmem16 = (u16 *)par->info->screen_base;
+	int x, y;
+	int ret = 0;
+
+	fbtft_par_dbg(DEBUG_WRITE_VMEM, par, "%s()\n", __func__);
+	write_reg(par, SEPS114A_DDRAM_DATA_ACCESS_PORT);
+
+	for (x = 0; x < par->info->var.xres; x++) {
+		for (y = 0; y < par->info->var.yres; y++) {
+			vmem16[y*par->info->var.xres+x] = swap_rgb(vmem16[y*par->info->var.xres+x]);
+		}
+	}
+
+	if (par->gpio.dc != -1)
+		gpio_set_value(par->gpio.dc, 1);
+
+	par->fbtftops.write(par, vmem16, par->info->var.xres*par->info->var.yres*2);
+
+	return ret;
 }
 
 
@@ -215,10 +212,12 @@ static struct fbtft_display display = {
 	.width = WIDTH,
 	.height = HEIGHT,
 	.fps = 95,
+	.bpp = 16,
 	.fbtftops = {
 		.reset = seps114a_display_reset,
 		.init_display = init_display,
 		.set_addr_win = set_addr_win,
+		.write_vmem = write_vmem
 	},
 };
 FBTFT_REGISTER_DRIVER(DRVNAME, "syncoam,seps114a", &display);
